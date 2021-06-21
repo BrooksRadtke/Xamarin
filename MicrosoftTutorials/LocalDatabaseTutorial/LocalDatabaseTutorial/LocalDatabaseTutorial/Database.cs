c6a6150767545a7ef7d1f5a6517e49ce545aee1e
Brooks Radtke <brooks@brooksradtke.com>
2021-06-21 14:18:53 -0700

    Add to repository

---
+++ MicrosoftTutorials/LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/Database.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace LocalDatabaseTutorial
+{
+    public class Database
+    {
+        public readonly SQLiteAsyncConnection database;
+
+        public Database(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Person>().Wait();
+        }
+
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            return database.Table<Person>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Person person)
+        {
+            return database.InsertAsync(person);
+        }
+    }
+}
